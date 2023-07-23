6801457983f205372b8fcdba53e2b17a50e90618
FlightSimFreak <108270108+FlightSimFreak@users.noreply.github.com>
2023-07-23 20:19:37 +1000

    Update ProjectSlayers.lua

--- ProjectSlayers.lua
+++ ProjectSlayers.lua
@@ -47,7 +47,7 @@ if game.PlaceId == 5956785391 then
             FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
             SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
             GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-            Key = {"https://pastebin.com/raw/GwP7dZqx"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+            Key = {"https://raw.githubusercontent.com/FlightSimFreak/AG-tq63afA/main/Script"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
         }
         })
 
@@ -276,7 +276,7 @@ if game.PlaceId == 5956785391 then
                             FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
                             SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
                             GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-                            Key = {"https://pastebin.com/raw/GwP7dZqx"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+                            Key = {"https://raw.githubusercontent.com/FlightSimFreak/AG-tq63afA/main/Script"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
                         }
                         })
             
@@ -1221,7 +1221,7 @@ if game.PlaceId == 6152116144 then
             FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
             SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
             GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-            Key = {"https://pastebin.com/raw/GwP7dZqx"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+            Key = {"https://raw.githubusercontent.com/FlightSimFreak/AG-tq63afA/main/Script"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
         }
         })
 
